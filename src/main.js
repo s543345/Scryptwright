f4b870129ef0537e856e3b92c10676dbf0444668
Chase Durbin <chase.durbin.02@gmail.com>
2025-02-05 14:48:22 -0600

    added basic electron app

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron/main')
+const path = require('node:path')
+
+function createWindow() {
+	const mainWindow = new BrowserWindow({
+		height: 500,
+		minHeight: 400,
+		minWidth: 500,
+		width: 900,
+		show: false,
+		titleBarStyle: "hidden",
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+	})
+
+	mainWindow.webContents.loadFile("index.html");
+
+	mainWindow.on("ready-to-show", () => {
+		mainWindow.show();
+	});
+
+  ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  })
+
+	mainWindow.webContents.ipc.on("toMain", (event, command, data) => {
+			mainWindow.setWindowLayout(mainWindow.getNativeWindowHandle(), data.sidebarWidth, data.titlebarHeight);
+	});
+}
+
+app.whenReady().then(() => createWindow());
+app.on("window-all-closed", () => app.quit());
