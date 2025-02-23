a95f29ba806f9d6559803754fdea1de5fcef6176
Elijah Booher <99350249+Ebooher21@users.noreply.github.com>
2025-02-23 17:41:41 -0600

    made a notes textarea for the notes frame and adjusted all frame sizes

--- src/renderer.js
+++ src/renderer.js
@@ -13,27 +13,40 @@ var hdfr = document.getElementById('header')
 var docfr = document.getElementById('docs')
 var comfr = document.getElementById('coms')
 var textArea = document.getElementById('document');
+var noteArea = document.getElementById('notes');
 var charnum = document.getElementById('charnum');
+
 hdfr.onload = function(){
-  hdfr.style.height = window.innerHeight + 'px';
-  hdfr.style.width = window.innerWidth + 'px';
+  hdfr.style.height = hdfr.contentWindow.document.body.scrollHeight+ 'px';
+  hdfr.style.width = hdfr.contentWindow.document.body.scrollHeight + 'px';
   hdfr.style.borderRadius = '15px';
 }
+
 docfr.onload = function(){
+  // frame adjustment and structure
   docfr.style.height = docfr.contentWindow.document.body.scrollHeight + 'px';
   docfr.style.width = docfr.contentWindow.document.body.scrollWidth + 'px';
   docfr.style.borderRadius = '15px';
 }
+
 comfr.onload = function(){
   comfr.style.height = comfr.contentWindow.document.body.scrollHeight + 'px';
   comfr.style.width = comfr.contentWindow.document.body.scrollWidth + 'px';
   comfr.style.borderRadius = '15px';
 }
+
 textArea.onload = function(){
   textArea.style.height = window.innerHeight * 0.8 + 'px';
   textArea.style.width = window.innerWidth * 0.5 + 'px';
   textArea.style.borderRadius = '15px';
 }
+
+noteArea.onload = function(){
+  textArea.style.height = window. innerHeight * 0.2 + 'px';
+  textArea.style.width = window.innerWidth * 0.5 + 'px';
+  textArea.style.borderRadius = '15px';
+}
+
 charnum.onload = function(){
   charnum.style.height = charnum.style.height + 'px';
   charnum.style.width = charnum.style.width + 'px';
