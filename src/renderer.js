5e1c8ad50b65714f5f1f150eb3da3d104be04a34
Elijah Booher <99350249+Ebooher21@users.noreply.github.com>
2025-02-23 02:09:24 -0600

    Entry widget that represents the writing area. needs resized to where it adjusts correctly to window resizing. also added a drop shadow to it and a word counter that I need to put inside the document also added animations to the frames

--- src/renderer.js
+++ src/renderer.js
@@ -7,19 +7,39 @@ document.getElementById('reset-to-system').addEventListener('click', async () =>
   await window.darkMode.system()
   document.getElementById('theme-source').innerHTML = 'System'
 })
+
 //frame size adjust to the window size
 var hdfr = document.getElementById('header')
 var docfr = document.getElementById('docs')
 var comfr = document.getElementById('coms')
+var textArea = document.getElementById('document');
+var charnum = document.getElementById('charnum');
 hdfr.onload = function(){
   hdfr.style.height = window.innerHeight + 'px';
   hdfr.style.width = window.innerWidth + 'px';
+  hdfr.style.borderRadius = '15px';
 }
 docfr.onload = function(){
   docfr.style.height = docfr.contentWindow.document.body.scrollHeight + 'px';
   docfr.style.width = docfr.contentWindow.document.body.scrollWidth + 'px';
+  docfr.style.borderRadius = '15px';
 }
 comfr.onload = function(){
   comfr.style.height = comfr.contentWindow.document.body.scrollHeight + 'px';
   comfr.style.width = comfr.contentWindow.document.body.scrollWidth + 'px';
+  comfr.style.borderRadius = '15px';
 }
+textArea.onload = function(){
+  textArea.style.height = window.innerHeight * 0.8 + 'px';
+  textArea.style.width = window.innerWidth * 0.5 + 'px';
+  textArea.style.borderRadius = '15px';
+}
+charnum.onload = function(){
+  charnum.style.height = charnum.style.height + 'px';
+  charnum.style.width = charnum.style.width + 'px';
+}
+
+//character counter
+textArea.addEventListener('input', function() {
+  charnum.textContent = this.value.length + " characters";
+})
