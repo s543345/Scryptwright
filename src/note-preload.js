5224f0a28077dce2c34efa8c9360f9c61a5585e3
s543345 <99349847+s543345@users.noreply.github.com>
2025-03-24 14:39:30 -0500

    Reintroduced preload context bridge for note editor

--- src/note-preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron/renderer')
-
-contextBridge.exposeInMainWorld(
-    "note", {
-      selectFileDialog: () => {return ipcRenderer.invoke('note:selectFileDialog')}, // calls selectFileDialog()
-      readFileContents: (filePath) => {return ipcRenderer.invoke('note:readFileContents', filePath)}, // calls readFileContents()
-      writeFileContents: (filePath, data) => {return ipcRenderer.invoke('note:writeFileContents', filePath, data)}
-    }
-  )
--- src/preload.js
+++ src/preload.js
@@ -5,3 +5,11 @@ contextBridge.exposeInMainWorld('darkMode', {
   system: () => ipcRenderer.invoke('dark-mode:system'),
   openSettings: () => ipcRenderer.send('open-settings')
 })
+
+contextBridge.exposeInMainWorld(
+  "note", {
+    selectFileDialog: () => {return ipcRenderer.invoke('note:selectFileDialog')}, // calls selectFileDialog()
+    readFileContents: (filePath) => {return ipcRenderer.invoke('note:readFileContents', filePath)}, // calls readFileContents()
+    writeFileContents: (filePath, data) => {return ipcRenderer.invoke('note:writeFileContents', filePath, data)}
+  }
+)
